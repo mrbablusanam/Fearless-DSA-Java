2e30c965871c317e633101180141c8d0d254f967
Md Badruddin <144376287+mrbablusanam@users.noreply.github.com>
2025-01-10 06:33:30 -0800

    Update Question_1.java

--- Assignments/Practice_Set_01/Question_1.java
+++ Assignments/Practice_Set_01/Question_1.java
@@ -3,7 +3,7 @@ package Assignments.Practice_Set_01;
 import java.util.Scanner;
 
 //Write a program to print whether a number is even or odd, also take input form the user
-public class Question_1 {
+public class Question_01 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number to Even or Odd:");
